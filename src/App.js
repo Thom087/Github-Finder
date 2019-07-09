64f2c5b520eca305d633181a846021234af5ff76
Thomas Schatt <thomas.schatt@bluewin.ch>
2019-07-09 22:12:23 +0200

    Feat: Event and Search Component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Navbar from './components/layout/Navbar';
 import Users from './components/users/Users';
+import Search from './components/users/Search';
 import axios from 'axios';
 import './App.css';
 class App extends Component {
@@ -10,8 +11,6 @@ class App extends Component {
   }
   //liefcycle methods, as soon as the component is mounted, this will be fired
   async componentDidMount() {
-    //Global variables inside .env.local
-    console.log(process.env.REACT_APP_GITHUB_CLIENT_ID);
     this.setState({ loading: true })
     const res = await axios.get(`https://api.github.com/users?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
     this.setState({ users: res.data, loading: false });
@@ -21,6 +20,7 @@ class App extends Component {
       <div className='App'>
         <Navbar title='Github Finder' icon='fab fa-github' />
         <div className='container'>
+          <Search />
           <Users loading={this.state.loading}
             users={this.state.users} />
         </div>
